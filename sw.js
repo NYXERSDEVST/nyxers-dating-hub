e77e1cc8afc76f646b585237c89afda7d77f0e91
NYXERS <dev@nyxers.local>
2026-09-20 10:15:53 +0200

    VELVET: add 12 fiction women + haus filter
    Haus first-names (Lára/Kass/Běla/Lina/Christel/Mara/Heike/Saskia) plus Romy/Kassandra/Nora/Freya. Gradient placeholders until REALISM stills.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='velvet-nyxers-v2-fiction-stills';
+const CACHE='velvet-nyxers-v3-cast-expand';
 const ASSETS=['./', './index.html', './manifest.webmanifest', 'icons/icon-192.png', 'icons/icon-512.png', './assets/generated/fatima_discover.png', './assets/generated/kayla_hub_cast.png', './assets/generated/lianne_chat_bg_v2.png', './assets/generated/velvet/velvet_lianne_neon_sisters.png', './assets/generated/velvet/velvet_caoimhe_niamh_sisters.png', './assets/generated/velvet/velvet_siobhan_discover.png', './assets/generated/sabine_discover.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
